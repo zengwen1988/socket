a84c585f28c291011b970e0292ee3a02d92d68e2
yuiwong <yuiwong@126.com>
2015-11-28 00:56:58 +0800

    For v2

--- xsocket/xsock_client_helper.cxx
+++ xsocket/xsock_client_helper.cxx
@@ -102,16 +102,15 @@ conn_fail:
 }
 
 int xsocket::SockClientHelper::startConnectByHostV2 (
-	const xsocket::NetProtocol& host,
+	xsocket::ip_t * ip,
+	xsocket::port_t * port,
 	uint32_t timeout, /* second */
 	xsocket::OnClientSocket * on_socket/* client socket callback */)
 {
 	int ret;
-	int sockfd;
 	xsocket::SockStartConnParams * params = NULL;
 
-	xlog::AppendV2(__func__, __FILE__, __LINE__, 0);
-
+	xlog::AppendV2(__func__, __FILE__, __LINE__, 0, XLOG_LEVEL_T);
 	/* check */
 	if (NULL == on_socket) {
 		ret = -xsocket::error::SOCKARG_INVAL;
@@ -119,34 +118,18 @@ int xsocket::SockClientHelper::startConnectByHostV2 (
 		return ret;
 	}
 
-	/* FIXME vaid ip first */
-
-	sockfd = xsocket::core::GetSockfdByHost(host);
-
-	if (sockfd < 0) {
-		ret = sockfd;
-		goto conn_fail;
-	} else {
-		ret = 0;
-	}
-
-	params = new SockStartConnParams(timeout, host, on_socket);
-	ret = xsocket::core::ClientHelper::startConnectBySockfdV2(sockfd, params);
-
+	params = new SockStartConnParams(timeout, ip, port, on_socket);
+	ret = xsocket::core::ClientHelper::startConnectBySockfdV2(params);
 	if (0 != ret) {
 		xlog::AppendV2("start connect 2 fail", __FILE__, __LINE__, ret);
-
-		goto conn2fail;
+		goto end;
 	} else {
 		/* success */
-		xlog::Append("start connect success");
+		xlog::AppendV2("start connect success", __FILE__, __LINE__, 0,
+			 XLOG_LEVEL_I);
 		return 0;
 	}
 
-conn2fail:
-	xsocket::core::CloseSocket(sockfd);
-	sockfd = -1;
-
-conn_fail:
+end:
 	return ret;
 }
