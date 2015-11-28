3819c2c37adbd1607bcb20c07a3fc8b59f0f8add
yuiwong <yuiwong@126.com>
2015-11-28 08:14:40 +0800

    On all log

--- xsocket/xsock_session_routine.cxx
+++ xsocket/xsock_session_routine.cxx
@@ -3,9 +3,7 @@
 #include <cstring>
 #include <posix/func/usleep.h>
 
-#if defined(XSOCKET_LOGLEVEL)
-#	include <x_logfile.hxx>
-#endif
+#include <x_logfile.hxx>
 
 #include <xsocket/sock_core.hxx>
 #include <xsocket/on_session.hxx>
@@ -17,9 +15,7 @@ xsocket::core::internal::SockSessionRoutine::SockSessionRoutine (int cli_fd,
 {
 	++instance_num;
 
-#if defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x20)
 	xlog::AppendV2(__func__, __FILE__, __LINE__, instance_num, XLOG_LEVEL_T);
-#endif
 
 	this->cli_fd = cli_fd;
 	this->svr_fd = svr_fd;
@@ -33,43 +29,33 @@ xsocket::core::internal::SockSessionRoutine::~SockSessionRoutine ()
 {
 	--instance_num;
 
-#if defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x20)
 	xlog::AppendV2(__func__, __FILE__, __LINE__, instance_num, XLOG_LEVEL_T);
-#endif
 }
 
 void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 {
-#if	defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x20)
 	xlog::AppendV2(__func__, __FILE__, __LINE__, 0, XLOG_LEVEL_T);
-#endif
 
 	xsocket::OnSession * os = this->on_session;
 	this->on_session = NULL;
 
 	if (NULL == os) {
-#if		defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x0)
 		xlog::AppendV2("FATAL: NULL on session. now exit", __FILE__,
 			__LINE__, 0, XLOG_LEVEL_F);
-#endif
 		return (void *)(long int)-EINVAL;
 	}
 
 	int cli_fd = this->cli_fd;
 	int svr_fd = this->svr_fd;
 
-#if	defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x30)
 	int i;
-#endif
 	int ret, rcved_bytes;
 	bool prev_ok;
 	struct timeval timeout_readable;
 	struct timeval timeout_rcv;
 	int nfds;
 	fd_set readfds;
-#if	defined(XSOCKET_LOGLEVEL)
 	char msg[128];
-#endif
 
 	xsocket::SockDidFinish didfi;
 	xsocket::SockRecved rcved;
@@ -92,9 +78,7 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 	}
 
 	if (tmi) {
-#if		defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x18)
 		xlog::AppendV2("user teminate", __FILE__, __LINE__, 0, XLOG_LEVEL_I);
-#endif
 		didfi.code = 0;
 		goto end;
 	}
@@ -116,27 +100,21 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 
 		if (-1 == ret) {
 			ret = errno;
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x8)
 			snprintf(msg, 127, "FAIL: select: %s. and will teminate",
 				strerror(ret));
 			msg[127] = '\0';
 			xlog::AppendV2(msg, __FILE__, __LINE__, ret, XLOG_LEVEL_E);
-#endif /* defined(XSOCKET_LOGLEVEL) ... */
 			didfi.code = -ret;
 			goto end;
 		} else if (0 == ret) {
 			/*
 			 * no ready and timetout
 			 */
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x30)
 			xlog::AppendV2("wait readable timeout", __FILE__, __LINE__, 0,
 				XLOG_LEVEL_XV);
-#endif
 		} else if (ret > 0) { /* XXX-FIXED: add > 0 */
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x28)
 			xlog::AppendV2("now readable", __FILE__, __LINE__, 0,
 				XLOG_LEVEL_V);
-#endif
 			prev_ok = true;
 		}
 
@@ -171,13 +149,11 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 
 				if (ret < 0) {
 					ret = errno;
-#if					defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x8)
 					snprintf(msg, 127,
 						"FAIL: setsockopt: %s. and will teminate",
 						strerror(ret));
 					msg[127] = '\0';
 					xlog::AppendV2(msg, __FILE__, __LINE__, ret, XLOG_LEVEL_F);
-#endif
 					didfi.code = -ret;
 					goto end;
 				} else {
@@ -189,9 +165,7 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 		/* recv */
 		rcved_bytes = -1;
 		if (prev_ok) {
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x28)
 			xlog::AppendV2("will recv", __FILE__, __LINE__, 0, XLOG_LEVEL_V);
-#endif
 			/*
 			 * to recv and wait till timeout
 			 *
@@ -209,35 +183,27 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 #endif
 
 			if ((ret < 0) && (EAGAIN == errno)) {
-#if				defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x10)
 				xlog::AppendV2("recv timeout", __FILE__, __LINE__, 0,
 					XLOG_LEVEL_W);
-#endif
 			} else if ((ret < 0) && (EWOULDBLOCK == errno)) {
-#if				defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x8)
 				xlog::AppendV2("EWOULDBLOCK: recv and will teminate",
 					__FILE__, __LINE__, 0, XLOG_LEVEL_W);
-#endif
 				didfi.code = -EWOULDBLOCK;
 				goto end;
 			} else if (ret <0) {
 				ret = errno;
-#if				defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x0)
 				snprintf(msg, 127,
 					"FAIL: recv: %s. and will teminate",
 					strerror(ret));
 				msg[127] = '\0';
 				xlog::AppendV2(msg, __FILE__, __LINE__, ret, XLOG_LEVEL_F);
-#endif
 				didfi.code = -ret;
 				goto end;
 			} else {
 				/* success */
 				rcved_bytes = ret;
-#if				defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x28)
 				xlog::AppendV2("recv success", __FILE__, __LINE__,
 				rcved_bytes, XLOG_LEVEL_V);
-#endif
 				prev_ok = true;
 			}
 		}
@@ -246,17 +212,14 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 		 * connection lost
 		 */
 		if (prev_ok && (0 == rcved_bytes)) {
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x10)
 				xlog::AppendV2("WARN: connection lost and will teminate",
 				__FILE__, __LINE__, 0, XLOG_LEVEL_W);
-#endif
 			didfi.code = 1;
 			goto end;
 		}
 
 		if (prev_ok && (rcved_bytes > 0)) {
 			/* Show when need */
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x30)
 			for (i = 0; i < rcved_bytes; ++i) {
 				fprintf(stdout, "0x%02x ", rcved.data[i]);
 				if (0 == ((i + 1) % 16)) {
@@ -267,7 +230,6 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 			if (0 != (i % 16)) {
 				fprintf(stdout, "\n");
 			}
-#endif
 			/*
 			 * tell/callback external
 			 */
@@ -281,10 +243,8 @@ void * xsocket::core::internal::SockSessionRoutine::run (void * /* nil */)
 			tmi = os->should_exit();
 		}
 		if (tmi) {
-#if			defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x18)
 			xlog::AppendV2("user teminate", __FILE__, __LINE__, 0,
 				XLOG_LEVEL_I);
-#endif
 			didfi.code = 0;
 			goto end;
 		}
@@ -301,11 +261,9 @@ end:
 	os->markExited(cli_fd);
 	os = NULL;
 
-#if	defined(XSOCKET_LOGLEVEL) && (XSOCKET_LOGLEVEL >= 0x18)
 	xlog::AppendV2(
 		"i'll exit(delete self) and NOT delete on session(will by server)",
 		__FILE__, __LINE__, 0);
-#endif
 	if (NULL != this->gc) {
 		this->gc->gc(this);
 	}
